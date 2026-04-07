074af1284cfdc1083bb83548293a1142a7f41710
arav20603 <dakshandaravind28@gmail.com>
2026-04-07 18:03:19 +0530

    Added backend render link

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -13,7 +13,8 @@ export default function Login() {
 
   const handleLogin = async () => {
     try {
-      const res = await fetch('http://localhost:5000/api/auth/login', {
+      // const res = await fetch('http://localhost:5000/api/auth/login', {
+      const res = await fetch('https://techmanas.onrender.com/api/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(form),
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -13,7 +13,8 @@ export default function Signup() {
 
   const handleSignup = async () => {
     try {
-      const res = await fetch('http://localhost:5000/api/auth/register', {
+      // const res = await fetch('http://localhost:5000/api/auth/register', {
+      const res = await fetch('https://techmanas.onrender.com/api/auth/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(form),
