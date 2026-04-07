6c6dddfe58a7069349f025449835ef66b877b9de
syndicate0ne <dakshandaravind28@gmail.com>
2026-04-07 17:42:53 +0530

    changed backend link

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -13,7 +13,7 @@ export default function Login() {
 
   const handleLogin = async () => {
     try {
-      const res = await fetch('https://aravportfolio.onrender.com/api/auth/login', {
+      const res = await fetch('http://localhost:5000/api/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(form),
